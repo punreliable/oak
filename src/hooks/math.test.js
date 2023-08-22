7cadc694017720c3a9fba69a1c4bd667859c701c
Brian Smith <briansmith@rocketcentral.com>
2023-08-22 13:30:29 -0400

    Added javascript scripts with basic unit tests to hooks dir

---
+++ src/hooks/math.test.js
@@ -0,0 +1,11 @@
+import { it, expect } from 'vitest';
+
+import { add } from './math';
+
+it( 'should summarize all number values in an array', () => {
+	const numbers = [1, 2, 3];
+	const result = add([1, 2, 3]);
+	expect(result).toBe(6);
+} );
+
+
