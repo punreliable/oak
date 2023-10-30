862367d2dc1c610c868e322fe2d634ccb8845a0c
Brian Smith <briansmith@rocketcentral.com>
2023-10-30 16:32:54 -0400

    Say Hello Oak!

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import * as bootstrap from "bootstrap";
 import "./App.scss";
 import "../node_modules/bootstrap/scss/bootstrap.scss";
-import RootLayout from "./pages/Root";
-import HomePage from "./pages/Home";
-import ResultsPage from "./pages/Results";
+//import RootLayout from "./pages/Root";
+//import HomePage from "./pages/Home";
+//import ResultsPage from "./pages/Results";
+import axios from 'axios';
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        {
-          path: "/",
-          element: <HomePage />,
-        },
-        {
-          path: "/results",
-          element: <ResultsPage />,
-        },
-      ],
-    },
-  ]);
+  
+  const number = Math.floor(Math.random() * 151) + 1;
+  const pokeNo = number.toString();
+  const requestURL = 'https://pokeapi.co/api/v2/pokemon/'+ pokeNo;
+  
+  const [pokemon, setPokemon] = useState(null);
+  
+  const prettyName = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+  
+  useEffect(() => {
+    axios.get( requestURL )
+    .then( res => { 
+      setPokemon(res.data) 
+      console.log(res.data) })
+    .catch( err => { console.log(err) })
+  }, [])
+    
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <RootLayout />,
+  //     children: [
+  //       {
+  //         path: "/",
+  //         element: <HomePage />,
+  //       },
+  //       {
+  //         path: "/results",
+  //         element: <ResultsPage />,
+  //       },
+  //     ],
+  //   },
+  // ]);
 
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      { pokemon && (
+        <h1>Hello, { prettyName( pokemon.name ) }!</h1>
+     
+      ) }
+      {/* <RouterProvider router={router} /> */}
     </div>
   );
 };
--- src/hooks/math.test.js
+++ src/hooks/math.test.js
@@ -8,4 +8,13 @@ it( 'should summarize all number values in an array', () => {
 	expect(result).toBe(6);
 } );
 
+it( 'should ', () => {
+	
+	const numbers = [1, 2, 3];
+	const result = add([1, 2, 3]);
+	expect(result).toBe(6);
+
+
+});
+
 
