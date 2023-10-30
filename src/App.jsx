f68c1d30fc9b90877e520179b37f659156fff39c
Brian Smith <briansmith@rocketcentral.com>
2023-10-30 17:53:21 -0400

    Added Oak graphic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import * as bootstrap from "bootstrap";
 import "./App.scss";
 import "../node_modules/bootstrap/scss/bootstrap.scss";
+import {professor} from "./assets/oak.svg";
 //import RootLayout from "./pages/Root";
 //import HomePage from "./pages/Home";
 //import ResultsPage from "./pages/Results";
@@ -47,10 +48,10 @@ const App = () => {
 
   return (
     <div className="App">
+      <img className="professorOak" src={professor} alt="Picture of Professor Oak" width="256" height="256" />
       { pokemon && (
         <h1>Hello, { prettyName( pokemon.name ) }!</h1>
-     
-      ) }
+      )}
       {/* <RouterProvider router={router} /> */}
     </div>
   );
