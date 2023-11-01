0890fab28b262f7d47f2931d07ca1956b69403fa
Brian Smith <briansmith@rocketcentral.com>
2023-11-01 13:28:02 -0400

    Professor Oak Appears and says hello to any Pokemon in the Kanto Region

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from "react";
 import * as bootstrap from "bootstrap";
 import "./App.scss";
 import "../node_modules/bootstrap/scss/bootstrap.scss";
-// import {professor} from "./assets/oak.svg";
+// import ProfessorOak from "./components/professorOak";
+
+import professorOak from "./assets/oak.svg";
 //import RootLayout from "./pages/Root";
 //import HomePage from "./pages/Home";
 //import ResultsPage from "./pages/Results";
 import axios from 'axios';
 import styled from "styled-components";
-import professorOak from "./assets/oak.png";
 
 const App = () => {
   
@@ -17,7 +18,13 @@ const App = () => {
   const pokeNo = number.toString();
   const requestURL = 'https://pokeapi.co/api/v2/pokemon/'+ pokeNo;
   
-  const [pokemon, setPokemon] = useState(null);
+  const [pokemon, setPokemon] = useState( null );
+  const [pokemonID, setPokemonID] = useState( null );
+  const [pokemonAbilities, setPokemonAbilities] = useState( null );
+  
+  // let pokemonMoves = {};
+  // let pokemonIMGs = {};
+  // let pokemonSpecies = {};
   
   const prettyName = (name) => {
     return name.charAt(0).toUpperCase() + name.slice(1);
@@ -26,47 +33,45 @@ const App = () => {
   useEffect(() => {
     axios.get( requestURL )
     .then( res => { 
-      setPokemon(res.data) 
-      console.log(res.data) })
+      setPokemon(res.data)
+      console.log(res.data)
+     })
     .catch( err => { console.log(err) })
   }, [])
-    
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <RootLayout />,
-  //     children: [
-  //       {
-  //         path: "/",
-  //         element: <HomePage />,
-  //       },
-  //       {
-  //         path: "/results",
-  //         element: <ResultsPage />,
-  //       },
-  //     ],
-  //   },
-  // ]);
+
+
 
   return (
     <div className="App">
-      <Professor>
-        <img className="professorOak" src={professorOak} alt="Picture of Professor Oak" width="256" height="256" />
-      </Professor>
       { pokemon && (
-        <h1>Hello, { prettyName( pokemon.name ) }!</h1>
+        <>
+        <div className="row">
+          <img src={professorOak} id="imgProfessorOak" alt="Professor Oak" height="128" width="128" />
+        </div>
+        <professorh1>
+          Hello, { prettyName( pokemon.name ) } <sup> #{pokemon.id}!</sup>
+        </professorh1>
+        </>
       )}
-      {/* <RouterProvider router={router} /> */}
     </div>
-  );
+  ); 
   
-  const Professor = styled.div`
-  background-size: cover;
-  background-repeat:norepeat;
-  background-position: center center;
-  height:256px;
-  width: 100%;
-  margin: 0 auto;
+  // const Professor = styled.div`
+  // background-size: cover;
+  // background-repeat:norepeat;
+  // background-position: center center;
+  // height:256px;
+  // width: 100%;
+  // margin: 0 auto;
+  // `;
+  
+  const professorh1 = styled.h1`
+  font-size: 3.2rem;
+  margin: 4rem;
+  sup {
+    font-size: 1.6rem;
+    top: -0.8rem;
+  }
   `;
   
 };
