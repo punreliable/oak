1a88402f1d6cd32080dc194e9cd5d270f93c4369
Brian <picketfence14@gmail.com>
2023-11-04 10:05:59 -0400

    Pokemon Type still causing issues, hidden

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "../node_modules/bootstrap/scss/bootstrap.scss";
 import prettyName from "./utilities/prettyName";
 // import ProfessorOak from "./components/professorOak";
 
-import { gsap } from "gsap/dist/gsap";  
+import { gsap } from "gsap/dist/gsap";
 import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin";
 
 gsap.registerPlugin(MotionPathPlugin);
@@ -21,11 +21,11 @@ import styled from "styled-components";
 import PokemonTypes from "./components/PokemonTypes";
 
 const App = () => {
-  
+
   const number = Math.floor(Math.random() * 151) + 1;
   const pokeNo = number.toString();
   const requestURL = 'https://pokeapi.co/api/v2/pokemon/'+ pokeNo;
-  
+
   const [pokemon, setPokemon] = useState( null );
   const [pokemonID, setPokemonID] = useState( null );
   const [pokemonAbilities, setPokemonAbilities] = useState( null );
@@ -33,17 +33,17 @@ const App = () => {
   const [pokemonWeight, setPokemonWeight] = useState( null );
   const [pokemonXP, setPokemonXP] = useState( null );
   const [pokemonTypes, setPokemonTypes] = useState( null );
-  
+
   let pokemonMoves = {};
   let pokemonIMGs = {};
   let pokemonSpecies = {};
-  
+
   let imageSrc = 'https://www.gravatar.com/avatar?s=15';
-  
-  
+
+
   useEffect(() => {
     axios.get( requestURL )
-    .then( res => { 
+    .then( res => {
       setPokemon(res.data)
       setPokemonHeight(res.data.height)
       setPokemonWeight(res.data.weight)
@@ -58,42 +58,40 @@ const App = () => {
     <div className="App">
       { pokemon && (
         <section className="nes-container">
-           
+
           <h1 className="oakHello">Hello,</h1>
           <h1 className="pokemonName">{ prettyName( pokemon.name ) }</h1>
-          
-          <img 
-            className="nes-avatar avatar pokemonAvatar" 
-            alt="Gravatar image example" 
+
+          <img
+            className="nes-avatar avatar pokemonAvatar"
+            alt="Gravatar image example"
             src={pokemon.sprites.front_default} />
-        
+
           <p>Height: {pokemon.height}in</p>
           <p>Weight: {pokemon.weight}lb</p>
           <p>Base XP: {pokemon.base_experience}xp</p>
-          
-          <PokemonTypes types={pokemon.types}></PokemonTypes>
 
           <section className="message-list" id="professorMessages">
-            
+
             <section className="message -right">
-              
+
               <div className="nes-balloon from-right">
                 <p>Entry #{pokemon.id} in <br />Kanto Pokedex!</p>
               </div>
               <div className="wrapperOak">
                 <img src={professorOak} id="imgProfessorOak" alt="Professor Oak" height="64" width="64" />
               </div>
-              
+
             </section>
-            
+
           </section>
-          
+
         </section>
-      
+
       )}
     </div>
-  ); 
-  
+  );
+
 };
 
 export default App;
--- src/components/PokemonTypes.jsx
+++ src/components/PokemonTypes.jsx
@@ -3,128 +3,28 @@ import "./PokemonTypes.scss";
 import prettyName from "../utilities/prettyName";
 
 const PokemonTypes = (props) => {
-	
+
 	console.log(props)
-	
+
 	let typeList = [];
-	
-	if(props.types[1]) {
-		
-		console.log('Second Type detected');
-		
-		let hasTwoTypes = true;
-		
-	} else {
-	
-		let hasTwoTypes = false;
-		console.log('Single Type Found');
-	}
-	
-	
-	// const personSalary = {
-	//   engineer: 1500,
-	//   programmer: 2500,
-	//   lawyer: 2000,
-	// }
-	const keys = Object.keys(props)
-	console.log(keys) // [engineer, programmer, lawyer]
 
-	console.log("Length: "+ props.length );
-// 
-// 	const printTypes = (props) => {
-// 		<div className="row">
-// 			<a href="#" className="nes-badge">
-// 			  <span className="is-dark">{prettyName(props.types[0].type.name)}</span>
-// 			</a>
-// 		
-// 		</div>
-// 		<div className="row">
-// 			<a href="#" className="nes-badge">
-// 			  <span className="is-dark">{prettyName(props.types[1].type.name)}</span>
-// 			</a>
-// 		</div>
-// 	};
-// 	for(let i = 0; i > 2; i++) {
-// 		if(hasTwoTypes) {
-// 			
-// 			printTypes(props);
-// 			
-// 		}
-// 	}
-	// const map = props => {
-	// 	
-	// 	props.map((typeList) => x * 2);
-	// 	
-	// }
-	
-
-	
-	//let isPropsAnArray = isArray(props);
-	// 
-	// let isPropsAnObject = isObject(props);
-	
-	
-	// 
-	// if( isPropsAnArray == true )  {
-	// 	
-	// 	console.log('Props is an object')
-	// 	
-	// } else {
-	// 	console.log('Props is NOT an object')
-	// }
-	// 
-	// 
-	// if( isPropsAnArray == true )  {
-	// 	
-	// 	console.log('Props is an array');
-	// 	for( let i = 0; i < 2; i++) { 
-	// 		
-	// 		typeCount = typeCount + 1
-	// 		
-	// 	}
-	//  			
- 	// } else {
-	// 	
-	// 	console.log('Props is not an array');
-	// 	 
-	//  }
-	//
-	// 
-	// 
-	// 
-	// console.log("Type Count: " + typeCount.toString() )
-	
-	
-	//let types = []
-
-	const newProps = props.forEach((element) => element.x)
-	console.log(newProps)
-
-	
-	console.log(prettyName(props.types[0].type.name))
-	
-	// const type = props => props.map(
-	// 	
-	// 	types.push(props.type.name)
-	// )
- 
 	return(
 		<>
 			{ props && (
-			
+
 			<div className="row">
 				<a href="#" className="nes-badge">
 	  			<span className="is-dark">{prettyName(props.types[0].type.name)}</span>
 				</a>
-			
+
 			</div>
-			
+
 			)}
 
 		</>
-		
+
 	);
-	
+
 };
 
 export default PokemonTypes;
