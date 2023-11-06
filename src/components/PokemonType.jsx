c01c8d5af59796a228b13ef3b96e89b8ed27cada
Brian Smith <briansmith@rocketcentral.com>
2023-11-06 14:44:12 -0500

    Pruned some un-necessary files

--- src/components/PokemonType.jsx
+++ src/components/PokemonType.jsx
@@ -1,21 +1,21 @@
 import { useState } from "React";
 import prettyName from "../utilities/prettyName";
 import "./PokemonType.scss";
+import { v4 as uuidv4 } from 'uuid';
+
+
+const getID = () => {
+	return uuidv4()
+};
 
 const PokemonType = (props) => {
 	
 	return(
-		
-		<>
 		<div className="row">
-			<a href="#" className="nesBadge" key={}>
+			<a href="#" className="nesBadge" key={getID}>
 			  <span className="isDark">{prettyName(props.types[0].type.name)}</span>
 			</a>
-		
 		</div>
-		<h1>Hello World</h1>
-		</>
-		
 	);
 	
 };
