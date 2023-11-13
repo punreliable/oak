58f2ef65737c19f7e1a039284dc9c7e337c61ded
Brian Smith <briansmith@rocketcentral.com>
2023-11-13 17:15:08 -0500

    Type List is working

--- src/components/PokemonTypeList.jsx
+++ src/components/PokemonTypeList.jsx
@@ -9,42 +9,35 @@ import { v4 as uuidv4 } from 'uuid';
 const PokemonTypeList = (props) => {
 	
 	console.log( props.types )
-	
+
 	let types = props.types
-	
+
 	console.log( types )
-	
-	let minified = types.map( 
-		x => prettyName(x.type.name)
-		
-	 )
+
+	let minified = types.map( x => 
+		<div className="row">
+			<a href="#" className="nes-badge" key={uuidv4()}>
+				<span className={`type-${x.type.name }`}>{ prettyName( x.type.name ) }</span>
+			</a>
+		</div>
+	)
+
+	let prettyMinified = types.map( x => prettyName(x.type.name) )
+
 	console.log("minified results: " + minified)
 	// let min = props.types.map( 
 	// 	( props.types ) => { props.types.name } 
 	// )
-	
 	// console.log( min )
-	
 	// console.log(props.types.map('type'))
-	
 	// const minimizedList = props.types.map( 'type', type.name );
-	
 	// console.log(minimizedList)
-	
-	
-	const pokemonTypeCounter = (props) => {
-		
-		return props.types.length > 1 ? 2 : 1;
-		
-	}
-	
-	return(
 
-		<div className="row">
-			<a href="#" className="nes-badge" key={uuidv4()}>
-				<span className="is-dark">{minified}</span>
-			</a>
-		</div>
+	return(
+		<>
+			{minified}
+
+		</>
 
 	);
 
---
+++ src/pages/SingleType.jsx
@@ -0,0 +1,12 @@
+
+
+const PageSingleType = (props) => {
+  return (
+    <>
+      <h1>Pokemon Type</h1>
+      <p>Coming Soon</p>
+    </>
+  );
+};
+
+export default ResultsPage;
