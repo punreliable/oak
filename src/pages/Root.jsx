e8c3f6e869b961b82e984c8871e4473cc763085a
Brian <picketfence14@gmail.com>
2024-01-07 11:43:13 -0500

    Root component reduced

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import styles from "./Root.module.scss";
-import Navbar from "../components/UI/Navbar";
+// import styles from "./Root.module.scss";
+// import Navbar from "../components/UI/Navbar";
 import Footer from "../components/UI/Footer";
 
 function RootLayout() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <main>
         <Outlet />
       </main>
