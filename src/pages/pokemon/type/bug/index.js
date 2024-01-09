83c8afb5b8deee2ee7e40e598eea982d775ff87b
Brian <picketfence14@gmail.com>
2024-01-08 22:48:47 -0500

    Testing pages dir

---
+++ src/pages/pokemon/type/bug/index.js
@@ -0,0 +1,13 @@
+// import './PagePokemonTypeBug.scss';
+
+const PagePokemonTypeBug = () => {
+	return(
+	<div className="container">
+		<div className="type-icon bug"></div>
+		<h1>Bug Type</h1>
+		<p>More details coming soon!</p>
+	</div>
+	);
+};
+
+export default PagePokemonTypeBug;
