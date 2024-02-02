8fc8ddf9fce88c6f9fef6474401786dff5b95fd9
Brian <picketfence14@gmail.com>
2024-02-01 23:37:55 -0500

    Kanto Page gives list of 1-50

--- src/components/dataFetching/PageKantoPokemon.jsx
+++ src/components/dataFetching/PageKantoPokemon.jsx
@@ -1,86 +1,30 @@
 import { useQuery } from '@tanstack/react-query'
 import styled from 'styled-components'
 import axios from 'axios'
+import prettyName from '../../utilities/prettyName'
+import PokemonList from './PokemonList'
+import MainNavigation from '../MainNavigation'
+// import './PageKantoPokemon.scss'
 
 const PageKantoPokemon = () => {
-	const pokemonId = 1
-	const { isLoading, error, data, status, isSuccess, isError } = useQuery({
-		queryKey: ['pokemon'],
-		queryFn: () => fetchPokemon(50),
-		staleTime: 1000 * 5, // 5 seconds
-		cacheTime: 1000 * 5,
-		refetchOnMount: false,
-		refetchOnWindowFocus: false,
-		refetchOnReconnect: true,
-		retry: 3,
-		retryDelay: 1000,
-		refetchInterval: 9000,
-	})
-	
-	// fetch Pokemon
-	const fetchPokemon = async (limit) => {
-		const response = await fetch(`https://pokeapi.co/api/v2/${[limit]}`)
-		if(!response.ok) {
-			throw new Error('Error using fetch')
-		}
-	}
 
-	// fetch Uknown
-	const fetchUnknown = async () => {
-		const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=50&limit=50`)
-		if(!response.ok) {
-			throw new Error('Error using fetch')
-		}
-	}
+	return(
+		<div className="nes-container">
+		<MainNavigation />
+		<h1>Kanto</h1>
+		<PokemonListWrapper>
+		<PokemonList />
+		</PokemonListWrapper>
+		<ContainerButtons>
+			<button type="button" className="nes-btn is-primary">Previous</button>
+				<span>&nbsp;</span>
+			<button type="button" className="nes-btn is-primary">Next</button>
+		</ContainerButtons>
+		</div>
 
-	const kantoList = useQuery({
-		queryKey: ['pokemon'],
-		queryFn: fetchPokemon
-	})
-
-	if(kantoList.isLoading || fetchUnknown.isLoading) {
-		return(<h1>
-			loading...
-		</h1>)
-	}
-	if( kantoList.error || fetchUnknown.error ) {
-		return(<h1>
-			Error: {error.message}
-		</h1>)
-	}
-
-	if(kantoList.data && response.ok) {
-
-		return(
-			<div className="container">
-				<h1>Kanto Pokemon</h1>
-				
-				{kantoList?.data.map(() => {
-					return(
-						<div className="nes-container with-title is-centered" key={kantoList.id}>
-							<p className="title">Name: {kantoList.name}</p>
-							<div className="nes-container is-rounded">
-								<img src={pokemons.sprites.front_default} alt={kantoList.name} />
-							</div>
-						</div>
-					)
-				})}
-
-				<div className="row">
-			
-					<ContainerButtons>
-						<button type="button" className="nes-btn is-primary">Previous</button>
-							<span>&nbsp;</span>
-						<button type="button" className="nes-btn is-primary">Next</button>
-					</ContainerButtons>
-
-				</div>
-
-			</div>
-
-		)
-	}
+	)
 }
+
 
 export default PageKantoPokemon
 
@@ -93,4 +37,8 @@ const ContainerButtons = styled.div`
 	justify-content: space-between;
 	width: 100%;
 	min-width: 420px;
-`
+`;
+
+const PokemonListWrapper = styled.div`
+	max-width: 420px;
+`;
