2ceb7ed2ea9fa05816df530e5d6ea43032aae680
Brian Smith <briansmith@rocketcentral.com>
2024-02-27 14:02:34 -0500

    WiP on Pokemon Species data

--- src/components/PagePokemonSpecies.jsx
+++ src/components/PagePokemonSpecies.jsx
@@ -2,6 +2,8 @@ import {useQuery} from '@tanstack/react-query'
 import {useState} from 'react'
 
 
+
+
 const [page, setPage] = useState(1);
 const {  data, isError, isLoading } = useQuery('pokemonSpecies', () => fetch(`https://pokeapi.co/api/v2/pokemon-species?limit=20&offset=${page}`).then(res => res.json()));
 
