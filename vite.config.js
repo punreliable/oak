4109d1c50b715cf5bb28a135def4a1d787eb544a
Brian <picketfence14@gmail.com>
2024-03-05 11:10:22 -0500

    All files to support Typescript added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
   test: {
     reporters: ['html']
   }
