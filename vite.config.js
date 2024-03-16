a82cb7b0f0bc2819c521383637dbadf9b5bb82fd
Brian <picketfence14@gmail.com>
2024-03-15 20:31:14 -0400

    Need to manually change file extensions, but should work

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
   test: {
     reporters: ['html']
   }
 })
+
+
