3842845e78b356b1945eca6c927fb2ddeb368b48
Brian <picketfence14@gmail.com>
2024-03-16 20:26:28 -0400

    SCSS Buttons updated, also, Moves render

--- src/components/PagePokemonTypeBug.tsx
+++ src/components/PagePokemonTypeBug.tsx
@@ -1,16 +1,103 @@
+import React, { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import MainNavigation from './MainNavigation'
+import axios from 'axios'
+import prettyName from '../utilities/prettyName'
 import './PagePokemonTypeBug.scss'
-import './MainNavigation'
-import MainNavigation from './MainNavigation';
+import type { Pokemon } from '../../pokemon.d.ts';
+
+const requestURL: string = `https://pokeapi.co/api/v2/type/bug`
+
+const styleTextAlignLeft: object = {
+	textAlign: 'left'
+}
+
+const fetchBugTypeInfo = async () => {
+
+	const response = await axios.get(requestURL)
+	if (response.status !== 200) {
+		throw new Error('Type could not be found.')
+	}
+
+	return response
+
+}
+
+const fetchBugTypePokemon = async (requestURL:string) => {
+
+	const response = await axios.get(requestURL)
+	if (response.status !== 200) {
+		throw new Error('Water type Pokemon could not be found.')
+	}
+	return response
+
+}
+
+
+
 
 const PagePokemonTypeBug = () => {
-	return(
-	<div className="container">
-		<MainNavigation />
-		<div className="type-icon bug"></div>
-		<h1>Bug Type</h1>
-		<p>More details coming soon!</p>
-	</div>
-	);
-};
 
-export default PagePokemonTypeBug;
+	const { data, isLoading, isError, error } = useQuery({
+		queryKey: ['type'],
+		queryFn: async () => {
+			const response = await axios.get(requestURL)
+			if (response.status !== 200) {
+				throw new Error('Bug type Pokemon could not be found.')
+			}
+			return response
+		}
+	})
+	
+	
+	if (isLoading) {
+		return(<div>Loading...</div>)
+	}
+	
+	if (isError) {
+		return(<div>Error: {error.message}</div>)
+	}
+
+	return(
+		<div className="nesContainer wrapper-bug">
+
+			<MainNavigation />
+
+			<div className="type-icon typeIcon bug"></div>
+
+			<h1 className="text-bug dark">Bug Type</h1>
+			
+			<section id="pokemon">
+				
+				<h2 className="text-bug dark">Pokemon</h2>
+
+				{
+					data?.data.pokemon.map((pokemon: any, index: number) => {
+						return (
+							<button type="button" className="nes-btn nesBtn isPrimary is-primary btnPokemon" key={index}>
+								{prettyName(pokemon.pokemon.name)}
+							</button>
+						)
+					})
+				}
+		
+			</section>
+
+		<section id="moves">
+			<h2>Moves</h2>
+
+			{
+				data?.data.moves.map((move: any, index: number) => {
+					return (
+						<button type="button" className="nes-btn nesBtn iserror btnPokemon" key={index}>
+							{prettyName(move.name)}
+						</button>
+					)
+				})
+			}
+		</section>
+		</div>
+	)
+}
+
+export default PagePokemonTypeBug
