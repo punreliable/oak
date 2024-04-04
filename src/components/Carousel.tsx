54c26e728a4b3531e2f8f43aa5fe39716ad0da46
Brian Smith <briansmith@rocketcentral.com>
2024-04-04 13:55:19 -0400

    Carousel is fully randomized

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,102 +5,87 @@ import { Pokemon } from '../../pokemon.js'
 import './Carousel.scss'
 import professorOak from '../assets/professorOak.svg'
 import unsurePokemon from '../assets/0.png'
-import { getRandomPokemon } from '../utilities/getRandomPokemon'
-
+import { getRandomKantoPokemon } from '../utilities/getRandomKantoPokemon'
 const fetchFirstPokemon = async () => {
-  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/1`)
+  const idOne = getRandomKantoPokemon()
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idOne}`)
   return response
 }
-
 const fetchSecondPokemon = async () => {
-  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/4`)
+  const idTwo = getRandomKantoPokemon()
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idTwo}`)
   return response
 }
-
 const fetchThirdPokemon = async () => {
-  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/7`)
+  const idThree = getRandomKantoPokemon()
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idThree}`)
   return response
 }
-
 const fetchFourthPokemon = async () => {
-  const idFour = getRandomPokemon()
+  const idFour = getRandomKantoPokemon()
   const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idFour}`)
   return response
 }
-
 const fetchFifthPokemon = async () => {
-  const idFive = getRandomPokemon()
+  const idFive = getRandomKantoPokemon()
   const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idFive}`)
   return response
 }
-
 const fetchSixthPokemon = async () => {
-  const idSix = getRandomPokemon()
+  const idSix = getRandomKantoPokemon()
   const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idSix}`)
   return response
 }
-
 const fetchSeventhPokemon = async () => {
-  const idSeven = getRandomPokemon()
+  const idSeven = getRandomKantoPokemon()
   const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idSeven}`)
   return response
 }
-
 const fetchEighthPokemon = async () => {
-  const idEight = getRandomPokemon()
+  const idEight = getRandomKantoPokemon()
   const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idEight}`)
   return response
 }
 
 const Carousel = () => {
-
 const defaultImage: string = unsurePokemon
 const queryClient = useQueryClient();
-
 const { data: firstPokemonData, isLoading: firstPokemonLoading, error: firstPokemonError, isError: firstPokemonIsError } = useQuery({
   queryKey: ['firstPokemon'],
   queryFn: () => fetchFirstPokemon(),
 })
-
 const { data: secondPokemonData, isLoading: secondPokemonLoading, error: secondPokemonError, isError: secondPokemonIsError } = useQuery({
   queryKey: ['secondPokemon'],
   queryFn: () => fetchSecondPokemon(),
 })
-
 const { data: thirdPokemonData, isLoading: thirdPokemonLoading, error: thirdPokemonError, isError: thirdPokemonIsError } = useQuery({
   queryKey: ['thirdPokemon'],
   queryFn: () => fetchThirdPokemon()
 
 })
-
 const { data: fourthPokemonData, isLoading: fourthPokemonLoading, error: fourthPokemonError, isError: fourthPokemonIsError } = useQuery({
   queryKey: ['fourthPokemon'],
   queryFn: () => fetchSixthPokemon(),
 })
-
 const { data: fifthPokemonData, isLoading: fifthPokemonLoading, error: fifthPokemonError, isError: fifthPokemonIsError } = useQuery({
   queryKey: ['fifthPokemon'],
   queryFn: () => fetchFifthPokemon(),
 })
-
 const { data: sixthPokemonData, isLoading: sixthPokemonLoading, error: sixthPokemonError, isError: sixthPokemonIsError } = useQuery({
   queryKey: ['sixthPokemon'],
   queryFn: () => fetchSixthPokemon()
 
 })
-
 const { data: seventhPokemonData, isLoading: seventhPokemonLoading, error: seventhPokemonError, isError: seventhPokemonIsError } = useQuery({
   queryKey: ['seventhPokemon'],
   queryFn: () => fetchSeventhPokemon()
 
 })
-
 const { data: eighthPokemonData, isLoading: eighthPokemonLoading, error: eighthPokemonError, isError: eighthPokemonIsError } = useQuery({
   queryKey: ['eighthPokemon'],
   queryFn: () => fetchEighthPokemon()
 
 })
-
 const [firstPokemon, setFirstPokemon] = useState<string>(defaultImage)
 const [secondPokemon, setSecondPokemon] = useState<string>(defaultImage)
 const [thirdPokemon, setThirdPokemon] = useState<string>(defaultImage)
@@ -109,45 +94,43 @@ const [fifthPokemon, setFifthPokemon] = useState<string>(defaultImage)
 const [sixthPokemon, setSixthPokemon] = useState<string>(defaultImage)
 const [seventhPokemon, setSeventhPokemon] = useState<string>(defaultImage)
 const [eighthPokemon, setEighthPokemon] = useState<string>(defaultImage)
-
-  useEffect(
-    () => {
-      if(firstPokemonData) {
-        setFirstPokemon(firstPokemonData.data.sprites.front_default)
-      }
-      if(secondPokemonData) {
-        setSecondPokemon(secondPokemonData.data.sprites.front_default)
-      }
-      if(thirdPokemonData) {
-        setThirdPokemon(thirdPokemonData.data.sprites.front_default)
-      }
-      if(fourthPokemonData) {
-        setFourthPokemon(fourthPokemonData.data.sprites.front_default)
-      }
-      if(fifthPokemonData) {
-        setFifthPokemon(fifthPokemonData.data.sprites.front_default)
-      }
-      if(sixthPokemonData) {
-        setSixthPokemon(sixthPokemonData.data.sprites.front_default)
-      }
-      if(seventhPokemonData) {
-        setSeventhPokemon(seventhPokemonData.data.sprites.front_default)
-      }
-      if(eighthPokemonData) {
-        setEighthPokemon(eighthPokemonData.data.sprites.front_default)
-      }
-    }, [
-      firstPokemonData, 
-      secondPokemonData, 
-      thirdPokemonData, 
-      fourthPokemonData, 
-      fifthPokemonData, 
-      sixthPokemonData, 
-      seventhPokemonData, 
-      eighthPokemonData
-    ]
-  )
-
+useEffect(
+  () => {
+    if(firstPokemonData) {
+      setFirstPokemon(firstPokemonData.data.sprites.front_default)
+    }
+    if(secondPokemonData) {
+      setSecondPokemon(secondPokemonData.data.sprites.front_default)
+    }
+    if(thirdPokemonData) {
+      setThirdPokemon(thirdPokemonData.data.sprites.front_default)
+    }
+    if(fourthPokemonData) {
+      setFourthPokemon(fourthPokemonData.data.sprites.front_default)
+    }
+    if(fifthPokemonData) {
+      setFifthPokemon(fifthPokemonData.data.sprites.front_default)
+    }
+    if(sixthPokemonData) {
+      setSixthPokemon(sixthPokemonData.data.sprites.front_default)
+    }
+    if(seventhPokemonData) {
+      setSeventhPokemon(seventhPokemonData.data.sprites.front_default)
+    }
+    if(eighthPokemonData) {
+      setEighthPokemon(eighthPokemonData.data.sprites.front_default)
+    }
+  }, [
+    firstPokemonData, 
+    secondPokemonData, 
+    thirdPokemonData, 
+    fourthPokemonData, 
+    fifthPokemonData, 
+    sixthPokemonData, 
+    seventhPokemonData, 
+    eighthPokemonData
+  ]
+)
   if(firstPokemonLoading) {
     return <div>Loading...</div>
   }
@@ -157,7 +140,6 @@ const [eighthPokemon, setEighthPokemon] = useState<string>(defaultImage)
   if(firstPokemonData) {
     console.log(firstPokemonData)
   }
-
   return(
     <>
       <div className="marquee marquee--8">
@@ -174,5 +156,4 @@ const [eighthPokemon, setEighthPokemon] = useState<string>(defaultImage)
     </>
   )
 }
-
 export default Carousel
