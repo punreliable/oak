40e2ef2548cb15e480846645ed694c9c1b6cbfed
Brian <picketfence14@gmail.com>
2024-04-06 11:57:52 -0400

    Page Carousel stylesheet created with scaling fonts

--- src/components/PagePokemonCarousel.tsx
+++ src/components/PagePokemonCarousel.tsx
@@ -5,11 +5,12 @@ import type { Pokemon } from '../../pokemon.js'
 import type { PokemonType } from '../../pokemon-type.js'
 import MainNavigation from './MainNavigation.js'
 import Carousel from './Carousel'
+import './PagePokemonCarousel.scss'
 
 const PagePokemonCarousel = () => {
 
 	return(
-		<div className="nesContainer">
+		<div className="nesContainer kantoCarousel">
 			<MainNavigation />
 			<h1 className="text-electric dark">Welcome to the World of Pokemon</h1>
 			<section id="carousel">
