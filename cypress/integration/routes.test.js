4adfe5595dbeabdb8347268a869757c8a6885ec2
Brian Smith <briansmith@rocketcentral.com>
2024-05-16 09:30:33 -0400

    Added Cypress dir

---
+++ cypress/integration/routes.test.js
@@ -0,0 +1,6 @@
+it("Displays correct heading when navigating to shows route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /shows/i }).click();
+  cy.findByRole("heading", { name: /upcoming shows/i }).should("exist")
+
+});
