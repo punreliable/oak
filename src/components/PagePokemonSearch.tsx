393014a94d7cc04629954344fbf2953f556fba8c
Brian <picketfence14@gmail.com>
2024-09-01 19:05:32 -0400

    I have built a Pokemon Search by Name Page

--- src/components/PagePokemonSearch.tsx
+++ src/components/PagePokemonSearch.tsx
@@ -7,10 +7,13 @@ import { FieldApi, useForm } from '@tanstack/react-form'
 import { PokemonModal } from "./PokemonModal"
 import MainNavigation from "./MainNavigation"
 import { closeSinglePokemonModal, showSinglePokemonModal } from "../utilities/modals"
-
-
+import prettyName from "../utilities/prettyName"
+import "./PagePokemonSearch.scss"
 
 export default function PagePokemonSearch() {
+
+  const [pokemon, setPokemon] = useState<any>(false);
+  const [enteredValue, setEnteredValue] = useState(false);
 
   function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
     return (
@@ -29,87 +32,82 @@ export default function PagePokemonSearch() {
 
     },
     onSubmit: async ({ value }) => {
-      // Do something with form data
-      console.log(value)
+      const name = value.name;
+      const results = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      setPokemon(results);
     },
   })
-  
 
   return (
-    <div>
-      <h1>Search by Name</h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            form.handleSubmit()
-          }}
-        >
-          <div>
-            {/* A type-safe field component*/}
-            <form.Field
-              name="firstName"
-              validators={{
-                onChange: ({ value }) =>
-                  !value
-                    ? 'A name is required'
-                    : value.length < 3
-                      ? 'First name must be at least 3 characters'
-                      : undefined,
-                onChangeAsyncDebounceMs: 500,
-                onChangeAsync: async ({ value }) => {
-                  await new Promise((resolve) => setTimeout(resolve, 1000))
+    <>
+        <section className="nesContainer nes-container">
+        <MainNavigation />
+        <h1>Search by Name</h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              form.handleSubmit()
+            }}
+          >
+            <div>
+              {/* A type-safe field component*/}
+              <form.Field
+                name="name"
+                validators={{
+                  onChange: ({ value }) =>
+                    !value
+                      ? 'A name is required'
+                      : value.length < 3
+                        ? 'Name must be at least 3 characters'
+                        : undefined,
+                  onChangeAsyncDebounceMs: 500,
+                  onChangeAsync: async ({ value }) => {
+                    await new Promise((resolve) => setTimeout(resolve, 1000))
+                    return (
+                      value.includes('error') &&
+                      'No "error" allowed in name'
+                    )
+                  },
+                }}
+                children={(field) => {
+                  // Avoid hasty abstractions. Render props are great!
                   return (
-                    value.includes('error') &&
-                    'No "error" allowed in first name'
+                    <div className="nes-field">
+                      {/* <label htmlFor={field.name}>Name:</label> */}
+                      <input
+                        className="nes-field"
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+
+                      />
+                      <FieldInfo field={field} />
+                    </div>
                   )
-                },
-              }}
-              children={(field) => {
-                // Avoid hasty abstractions. Render props are great!
-                return (
-                  <>
-                    <label htmlFor={field.name}>Name:</label>
-                    <input
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                    <FieldInfo field={field} />
-                  </>
-                )
-              }}
-            />
-          </div>
-          {/* <div>
-            <form.Field
-              name="lastName"
-              children={(field) => (
-                <>
-                  <label htmlFor={field.name}>Last Name:</label>
-                  <input
-                    id={field.name}
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                  <FieldInfo field={field} />
-                </>
+                }}
+              />
+            </div>
+            <form.Subscribe
+              selector={(state) => [state.canSubmit, state.isSubmitting]}
+              children={([canSubmit, isSubmitting]) => (
+                <button id="submit" type="submit" disabled={!canSubmit} className="nes-btn is-primary">
+                  {isSubmitting ? '...' : 'Submit'}
+                </button>
               )}
             />
-          </div> */}
-          <form.Subscribe
-            selector={(state) => [state.canSubmit, state.isSubmitting]}
-            children={([canSubmit, isSubmitting]) => (
-              <button type="submit" disabled={!canSubmit} className="nes-btn is-primary">
-                {isSubmitting ? '...' : 'Submit'}
-              </button>
-            )}
-          />
-        </form>
-    </div>
+          </form>
+          {pokemon.data && (
+      
+      <div id="results">
+        <p>
+          { prettyName(`${pokemon.data.name}`)} is pokemon #{`${pokemon.data.id}`}
+        </p>
+      </div>
+    )}
+      </section>
+      </>
   )
 }
