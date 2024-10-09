316922323dedb2da4cae89b745bbd451625c6849
Brian Smith <briansmith@rocketcentral.com>
2024-10-09 17:04:04 -0400

    Now rendering as a Nextjs app

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} nes-container antialiased`}
+        /* className={`${geistSans.variable} ${geistMono.variable} antialiased`} */ 
       >
         {children}
       </body>
