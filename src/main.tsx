f2fa3a93dd8a9312c808dae8b8cd850a22084ef4
Brian <punreliable@gmail.com>
2024-11-24 21:32:56 -0500

    App running again after bug found

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
