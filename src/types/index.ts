daf9100b1cb3210de05b5afabf9b07f297d3bf8c
Brian <punreliable@gmail.com>
2024-11-29 23:11:45 -0500

    SVG is now a Typescript Type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,14 @@
 import type {Pokemon} from './pokemon.d.ts'
 import type {PokemonType} from './pokemon-type.d.ts'
+import type { SVG } from './svg.d.ts'
 
 export type Pokemon;
 export type PokemonType;
+export type SVG;
 
 export type Types = [
   Pokemon,
   PokemonType,
+  SVG,
   ...any[]
 ];
