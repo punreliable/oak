bd0fe9f43f1d3f7053b21fd04d7bd363faed3bdd
Brian <punreliable@gmail.com>
2025-01-06 14:48:04 -0500

    Fixed Parser hook

--- src/hooks/parser.ts
+++ src/hooks/parser.ts
@@ -1,6 +1,6 @@
-export function extractNumbers(formData) {
+export function extractNumbers(formData: FormData): [string, string] {
   const num1Input = formData.get('num1');
   const num2Input = formData.get('num2');
 
-  return [num1Input, num2Input];
+  return [num1Input as string || '', num2Input as string || ''];
 }
