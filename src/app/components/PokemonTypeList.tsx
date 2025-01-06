a0533b1357c2aa286e4bbaa229e7e606c7e0c544
Brian <punreliable@gmail.com>
2025-01-06 14:31:05 -0500

    Pokemon Type List matches Pokemon Move List

--- src/app/components/PokemonTypeList.tsx
+++ src/app/components/PokemonTypeList.tsx
@@ -4,7 +4,7 @@ import prettyName from '../../utilities/prettyName';
 import { v4 as uuidv4 } from 'uuid';
 import './PokemonTypeList.scss';
 
-const PokemonTypeList = (props) => {
+const PokemonTypeList = (props: any) => {
   let types = props.types;
   let count: number = props.types.length;
 
@@ -12,7 +12,7 @@ const PokemonTypeList = (props) => {
   let halfColumn = 'col-md-6 col-sm-12';
 
   let columns = count > 1 ? halfColumn : fullColumn;
-  let minified = types.map((x) => (
+  let minified = types.map((x: any) => (
     <div className={columns} key={uuidv4()}>
       <a
         href={`/pokemon/type/${x.type.name}`}
