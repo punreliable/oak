a7c8fc59856ea18ec5e0cd79486438309c94463c
Brian <punreliable@gmail.com>
2025-01-07 14:06:15 -0500

    Added SpeedInsights component from Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.scss';
 import { TanstackProvider } from './components/providers/tanstack-provider';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -77,6 +78,7 @@ export default function RootLayout({
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <TanstackProvider>{children}</TanstackProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
