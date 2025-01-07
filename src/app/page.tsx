62a2f0e8694b417a54e9e75332353fa472a57a05
Brian <punreliable@gmail.com>
2025-01-07 13:01:44 -0500

    I think I've fixed the routing?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-import OtherHome from '../app/(pages)/Home/index';
+'use client';
+import Image from 'next/image';
+// import MainNavigation from "../components/MainNavigation";
+// import { Main } from "next/document";
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+// import MainNavigation from "./components/MainNavigation";
+import Error from './(pages)/Home/Error';
+import Pending from './(pages)/Home/Pending';
+import Result from './(pages)/Home/Result';
+// import "./App.scss";
 
-export default function Home() {
+const getPokemonNumber = () => {
+  const number = Math.floor(Math.random() * 151) + 1;
+  if (number < 10) {
+    return `0${number}`;
+  }
+  return `${number}`;
+};
+
+const Home = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['pokemon'],
+    queryFn: async () => {
+      const id = getPokemonNumber();
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      return response.data;
+    },
+  });
+
+  if (isLoading) return <Pending />;
+
+  if (error) return <Error />;
+
   return (
-    <>
-      <OtherHome />
-    </>
+    <div className='App'>
+      <section className='nesContainer nes-container'>
+        {/* <MainNavigation /> */}
+        {data && <Result pokemon={data} />}
+      </section>
+    </div>
   );
-}
+};
+
+export default Home;
