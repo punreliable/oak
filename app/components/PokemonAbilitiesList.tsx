49e2dddae5477dcc0fdd5f0432572a027b29dc13
Brian <punreliable@gmail.com>
2025-03-04 12:09:38 -0500

    Re-routing move and ability to not redirect to /pokemon/*

--- app/components/PokemonAbilitiesList.tsx
+++ app/components/PokemonAbilitiesList.tsx
@@ -8,7 +8,7 @@ const PokemonAbilitiesList = (props: any) => {
   const minified = abilities.map((x: any) => (
     <div className='col-sm-12 col-md-6 col-lg-3' key={uuidv4()}>
       <a
-        href={`/pokemon/ability/${x.ability?.name}`}
+        href={`/ability/${x.ability?.name}`}
         className={`nes-btn ability-${x.ability?.name} is-${x.ability?.name}`}
       >
         <span className={`is-${x.ability?.name}`}>{prettyName(x.ability.name)}</span>
--- app/components/PokemonMoveList.tsx
+++ app/components/PokemonMoveList.tsx
@@ -8,7 +8,7 @@ const PokemonMoveList = (props: any) => {
   const minified = moves.map((x: any) => (
     <div className='col-sm-12 col-md-6 col-lg-3' key={uuidv4()}>
       <a
-        href={`/pokemon/move/${x.move.name}`}
+        href={`/move/${x.move.name}`}
         className={`nes-btn move-${x.move.name} is-${x.move.name}`}
       >
         <span className={`is-${x.move.name}`}>{prettyName(x.move.name)}</span>
