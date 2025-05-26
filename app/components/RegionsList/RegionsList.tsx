03947e563e6bea3e7f7de3e7bd6717273db0c376
Brian <punreliable@gmail.com>
2025-05-26 14:20:58 -0400

    RegionsList should work now

--- app/components/RegionsList/RegionsList.tsx
+++ app/components/RegionsList/RegionsList.tsx
@@ -21,6 +21,9 @@ const RegionsList: React.FC<RegionsListProps> = ({ regions }) => {
   const output = regions.map((region: { key: number | string; name: React.Key | null | undefined; url: string; }) => (
   content.push( {'key': uuidv4(), 'name': region.name, 'url': region.url })
   ));
+  
+  console.log('Output: ', output);
+
   return (
     <section className='nesContainer nes-container'>
     <div>
