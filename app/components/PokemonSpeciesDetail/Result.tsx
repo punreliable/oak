5d01f90750bf1c1cb363eb8be085ac0342be97f0
Brian <punreliable@gmail.com>
2025-05-26 13:50:38 -0400

    Simplified component with issues

--- app/components/PokemonSpeciesDetail/Result.tsx
+++ app/components/PokemonSpeciesDetail/Result.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import Error from './Error';
-import Pending from './Pending';
-import PokemonTile from '../PokemonTile/PokemonTile';
 
-const Result = (props: any) => {
+const Result = ( props: { pokedex: {}; pokedexes: [ url: string ] } ) => {
 
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['pokedexes', props.pokedex.pokedexes[0].url],
-    queryFn: async () => {
-      const res = await axios.get(`${props.pokedex.pokedexes[0].url}`);
-      return res.data;
-    },
-  });
-  
-  if (isLoading) return <Pending />;
-  if (isError) return <Error />;
-  if (data) return (
-    <>
-      <PokemonTile data={data.pokemon_entries[0].pokemon_species.url} />
-
-    </>
-  );
+  return (<h1>Coming Soon</h1>);
 
 };
 
