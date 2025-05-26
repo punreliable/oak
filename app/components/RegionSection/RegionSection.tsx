036f30e797ad5bd66d4946a08826b3cab847405e
Brian <punreliable@gmail.com>
2025-05-26 14:17:08 -0400

    Removed Unused component

--- app/components/RegionSection/RegionSection.tsx
+++ app/components/RegionSection/RegionSection.tsx
@@ -1,26 +1,11 @@
 'use client'; // Add this if it's a client component
 import React from 'react';
-import PokedexByRegion from '@/app/components/PokedexByRegion/PokedexByRegion';
-import { v4 as uuidv4 } from 'uuid';
 
-interface RegionSectionProps {
-
-  data: PokemonNameUrl[]; // Using the PokemonNameUrl interface
-  regionName?: string; // Example: if the region name is also a prop
-  regionUrl?: string; // Example: if the region url is also a prop
-}
-export interface PokemonNameUrl {
-  name: string;
-  url: string;
-}
-
-const RegionSection: React.FC<RegionSectionProps> = ( props: RegionSectionProps ) => {
-
-  const data = props.data;
+const RegionSection= () => {
 
   return (
     <div className="region-section">
-      {data.map( data => <PokedexByRegion key={uuidv4()} url={data.url} /> )}
+      <h1>Coming Soon.</h1>
     </div>
   );
 
