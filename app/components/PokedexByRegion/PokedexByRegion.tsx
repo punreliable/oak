f5e93fd06d8f6789a5a18c5a3ccaab6d17b32157
Brian <punreliable@gmail.com>
2025-05-26 14:15:08 -0400

    Removed another unused component

--- app/components/PokedexByRegion/PokedexByRegion.tsx
+++ app/components/PokedexByRegion/PokedexByRegion.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import Error from './Error';
-import Pending from './Pending';
-import StarterPokemon from '@/app/components/StarterPokemon/StarterPokemon';
 
-const PokedexByRegion = (props: {key: string; url: string;}) => {
+const PokedexByRegion = () => {
 
-	const url = props.url;
-  console.log("Region URL: ", props.url);
-	const { data, error, isLoading } = useQuery({
-    queryKey: ['pokedex', url],
-    queryFn: async () => {
-      const response = await axios.get(`${url}`);
-      return response.data;
-    },
-  });
 
-  if (isLoading) return <Pending />;
-  
-  if (error) return <Error />;
 
-  if(data) {
     return (
       <section className='nesContainer nes-container'>
-        <StarterPokemon pokedex={data} />
+        <h1>Coming Soon.</h1>
       </section>
     );
-  }
+  
   
 }
 
