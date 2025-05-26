67266cc1d46c3fc7cabdb1beca6a170e228b80b4
Brian <punreliable@gmail.com>
2025-05-26 14:13:21 -0400

    Removed unused component by simplifying it

--- app/components/RegionsList/Result.tsx
+++ app/components/RegionsList/Result.tsx
@@ -1,28 +1,7 @@
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import Error from './Error';
-import Pending from './Pending';
-import RegionPokedex from '../RegionPokedex/RegionPokedex';
 
-const Result = (props: any) => {
-
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['pokedexes'],
-    queryFn: async () => {
-      const res = await axios.get(`${props.pokedex.pokedexes[0].url}`);
-      return res.data;
-    },
-  });
-  
-  if (isLoading) return <Pending />;
-  if (isError) return <Error />;
-  if (data) return (
-    <>
-      <RegionPokedex pokedexUrl={data.pokemon_entries[0].pokemon_species.url} />
-    </>
-  );
-
+const Result = () => {
+  return <h1>Coming Soon.</h1>;
 };
 
 export default Result;
