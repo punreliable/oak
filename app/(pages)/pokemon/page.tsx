adfc744fa7a5115fa54f99b45bde07e89165c766
Brian <punreliable@gmail.com>
2025-06-20 09:58:44 -0400

    Page Layout updates on form submission

--- app/(pages)/pokemon/page.tsx
+++ app/(pages)/pokemon/page.tsx
@@ -6,10 +6,12 @@ const PokemonPage = () => {
 
 const [pokemon, setPokemon] = useState<string|null>(null);
 const [inputIdentifier, setInputIdentifier] = useState<string>('');
+const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
 
 const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
   event.preventDefault();
   setPokemon(inputIdentifier);
+  setFormSubmitted(true);
 };
 
 const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,67 +20,76 @@ const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 
 return(
 
+
+
   <div id="main">
+    
+    {!formSubmitted && (
 
-    <div className="feature-image" style={{"backgroundImage": "url('/images/whos-that-pokemon.png');"}}>
+      <div className="feature-image" style={{"backgroundImage": "url('/images/whos-that-pokemon.png');"}}>
 
-      <div className="mask rgba-black-strong d-flex justify-content-center align-items-top">
+        <div className="mask rgba-black-strong d-flex justify-content-center align-items-top">
 
-        <main className="wow fadeIn content gameboy-screen">
+          <main className="wow fadeIn content gameboy-screen">
 
-          <h1 className="h1-responsive mb-4 pt-4 px-2 gameboy text-center font-weight-bold">Who's that Pokémon?</h1>
+            <h1 className="h1-responsive mb-4 pt-4 px-2 gameboy text-center font-weight-bold">Who's that Pokémon?</h1>
 
-          <h2 className="h4-responsive gameboy text-center py-4 px-2">Learn more about Pokémon</h2>
+            <h2 className="h4-responsive gameboy text-center py-4 px-2">Learn more about Pokémon</h2>
 
-          <hr className="py-2" />
+            <hr className="py-2" />
 
-          <p className="d-none d-md-block text-center pb-4 px-2 gameboy">
-            Search by Pokémon name or number:
-          </p>
+            <p className="d-none d-md-block text-center pb-4 px-2 gameboy">
+              Search by Pokémon name or number:
+            </p>
+            
+            <form
+              onSubmit={handleSubmit}
+              name="pokemon-by-number"
+              id="pokemon-by-number"
+              className="pokemon-by-number my-4 mx-4">
 
-          <form
-            onSubmit={handleSubmit}
-            name="pokemon-by-number"
-            id="pokemon-by-number"
-            className="pokemon-by-number my-4 mx-4">
 
-            <div className="md-form form-group mx-2">
+              <div className="md-form form-group mx-2">
 
-              <div className="nes-field">
-                <label htmlFor="pokemon">Enter Name or Number</label>
-                <input 
-                  type="text" 
-                  className="nes-input form-control gameboy" 
-                  id="pokemon" 
-                  name="pokemon" 
-                  onChange={handleInputChange} 
-                  value={inputIdentifier} 
-                  required
-                />
+                <div className="nes-field">
+                  <label htmlFor="pokemon">Enter Name or Number</label>
+                  <input 
+                    type="text" 
+                    className="nes-input form-control gameboy" 
+                    id="pokemon" 
+                    name="pokemon" 
+                    onChange={handleInputChange} 
+                    value={inputIdentifier} 
+                    required
+                  />
+                </div>
+
+                <label 
+                  htmlFor="pokemon"
+                  data-error="wrong"
+                  data-success="right">
+                  Enter Name or Number
+                </label>
+
               </div>
 
-              <label 
-                htmlFor="pokemon"
-                data-error="wrong"
-                data-success="right">
-                Enter Name or Number
-              </label>
+              <div className="md-form form-group text-center pt-4">
+                <button type="submit" className="nes-btn is-primary gameboy white-text">Search</button>
+              </div>
 
-            </div>
+            </form>
 
-            <div className="md-form form-group text-center pt-4">
-              <button type="submit" className="nes-btn is-primary gameboy white-text">Search</button>
-            </div>
+              
 
-          </form>
+            </main>
 
-            {pokemon && <WhoseThatPokemon pokemon={pokemon} />}
-
-          </main>
-
-        </div>
+          </div>
 
       </div>
+
+    )}
+    
+    {pokemon && <WhoseThatPokemon pokemon={pokemon} />}
 
     </div>
   );
