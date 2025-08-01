eaef501b0799bf73dc266d620eaa6595e9866df7
Brian <punreliable@gmail.com>
2025-08-01 13:02:44 -0400

    WiP, but refactoring of PokemonTypeDamageRelations component

--- app/components/DamageRelations/DoubleDamageFrom.tsx
+++ app/components/DamageRelations/DoubleDamageFrom.tsx
@@ -1,12 +1,24 @@
 import { DoubleDamageFromProps } from '@/types/DamageRelations';
+import prettyName from '@/utilities/prettyName';
 
-const DoubleDamageFrom = ({double_damage_from}: DoubleDamageFromProps) => {
+const DoubleDamageFrom = (double_damage_from: DoubleDamageFromProps) => {
 
-	const doubleDamageFrom = double_damage_from;
+	const doubleDamageFrom = double_damage_from || [];
+
+	// console.log("Double Damage From", props.double_damage_from);
+
+	// const listOfDoubleDamageFromTypes = doubleDamageFrom.map((x: any) => {
+	// 	<div key={x.double_damage_from.name} className="col-md-2">
+	// 		<a className="btn btn-primary" href={`/type/${x.double_damage_from.name}`}>
+	// 			{prettyName(x.double_damage_from.name)}
+	// 		</a>
+	// 	</div>
+	// 	});
 
 	return(
 		<>
 		<h3>Double Damage From</h3>
+		{/* {listOfDoubleDamageFromTypes} */}
 		</>
 	);
 }
--- app/components/PokemonTypeDamageRelations.tsx
+++ app/components/PokemonTypeDamageRelations.tsx
@@ -1,18 +1,30 @@
 import DoubleDamageFrom from '@/app/components/DamageRelations/DoubleDamageFrom';
-import DoubleDamageTo from '@/app/components/DamageRelations/DoubleDamageTo';
+// import DoubleDamageTo from '@/app/components/DamageRelations/DoubleDamageTo';
 
-const PokemonTypeDamageRelations = ( damage_relations:  any 
-	// double_damage_from: [], 
-	// double_damage_to: []
+const PokemonTypeDamageRelations = ( props: any
+	// props: {
+	// 	damage_relations: { 
+	// 		double_damage_from: [{name: string; url: string;}], 
+	// 		double_damage_to: [{name: string; url: string;}]
+	// 	},
+	// }
 ) => {
+
+	console.log('Props: ', props);
+	const doubleDamageFrom: any = props.damageRelations?.double_damage_from;
+
+	const name = props.damageRelations.double_damage_from[0].name;
+	let listOfDoubleDamageFrom = doubleDamageFrom.map(
+		buildList(doubleDamageFrom)
+	);
+
+	function buildList(x: any) {
+		return <a href={`/type/${x.name}`}>${x.name}</a>;
+	}
+		
 	return(
 		<div className="row">
-			<div className="col-lg-4 col-md-4">
-				<DoubleDamageFrom double_damage_from={damage_relations.double_damage_from} />
-			</div>
-			<div className="col-lg-4 col-md-4">
-				<DoubleDamageTo double_damage_to={damage_relations.double_damage_to} />
-			</div>
+			{doubleDamageFrom && <div className="col-lg-4 col-md-12">{ doubleDamageFrom.damageRelations && listOfDoubleDamageFrom }</div>}
 		</div>
 	);
 }
