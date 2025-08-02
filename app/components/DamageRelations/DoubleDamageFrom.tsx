0af2a001f9c9771e32601c8e1b7bcfeca6f91483
Brian <punreliable@gmail.com>
2025-08-02 14:19:22 -0400

    Properly Linted app

--- app/components/DamageRelations/DoubleDamageFrom.tsx
+++ app/components/DamageRelations/DoubleDamageFrom.tsx
@@ -1,18 +1,20 @@
 import { DoubleDamageFromProps } from '@/types/DamageRelations';
 import prettyName from '@/utilities/prettyName';
 
-const DoubleDamageFrom = (double_damage_from: DoubleDamageFromProps) => {
-  const doubleDamageFrom = double_damage_from || [];
+const DoubleDamageFrom = (double_damage_from: DoubleDamageFromProps[]) => {
+  const doubleDamageFrom = double_damage_from;
 
-  // console.log("Double Damage From", props.double_damage_from);
+  console.log('Double Damage From', double_damage_from);
 
-  const listOfDoubleDamageFromTypes = double_damage_from.map((x: any) => {
+  const listOfDoubleDamageFromTypes = doubleDamageFrom.map((x: any) => {
     <div key={x.double_damage_from.name} className='col-md-2'>
       <a className='btn btn-primary' href={`/type/${x.double_damage_from.name}`}>
         {prettyName(x.double_damage_from.name)}
       </a>
     </div>;
   });
+
+  console.log('List of Double Damage from Types: ', listOfDoubleDamageFromTypes);
 
   return (
     <>
--- app/components/PokemonTypeDamageRelations.tsx
+++ app/components/PokemonTypeDamageRelations.tsx
@@ -1,16 +1,14 @@
+import DoubleDamageFrom from '@/app/components/DamageRelations/DoubleDamageFrom';
 const PokemonTypeDamageRelations = (props: any) => {
   const doubleDamageFrom: any = props.damageRelations?.double_damage_from;
-  const listOfDoubleDamageFrom = doubleDamageFrom.map(buildList(doubleDamageFrom));
-
-  function buildList(x: any) {
-    return <a href={`/type/${x.name}`}>${x.name}</a>;
-  }
 
   return (
     <div className='row'>
       {doubleDamageFrom && (
         <div className='col-lg-4 col-md-12'>
-          {doubleDamageFrom.damageRelations && listOfDoubleDamageFrom}
+          {doubleDamageFrom.damageRelations && (
+            <DoubleDamageFrom double_damage_from={doubleDamageFrom} />
+          )}
         </div>
       )}
     </div>
