8137a38d038a40ecb5420f9e5b3f2b168006e12b
Brian <punreliable@gmail.com>
2025-08-05 17:07:56 -0400

    Fairy Type Page created

--- app/(pages)/type/electric/page.tsx
+++ app/(pages)/type/electric/page.tsx
@@ -1,9 +1,29 @@
+'use client';
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import Error from './Error';
+import Pending from './Pending';
+import Result from './Result';
 const PokemonTypeElectricPage = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['type'],
+    queryFn: async () => {
+      const id: string = 'bug';
+      const response = await axios.get(`https://pokeapi.co/api/v2/type/${id}`);
+      return response.data;
+    },
+  });
+  if (isLoading) return <Pending />;
+  if (error) return <Error />;
+
   return (
-    <>
-      <h1>Electric Type</h1>
-      <h3>More Details Coming Soon.</h3>
-    </>
+    <section className='App container container-type-electric'>
+      <div className='row'>
+        <h1 className='responsive-h1 text-center gameboy title'>Electric Type</h1>
+      </div>
+      {data && <Result damage_relations={data} />}
+    </section>
   );
 };
 
