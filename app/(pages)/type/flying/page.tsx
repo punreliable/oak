6b4441a7f08a43e0126e89392e01e1a4375cb0d9
Brian <punreliable@gmail.com>
2025-08-05 13:26:10 -0400

    Bug type copied to Water and Flying

--- app/(pages)/type/flying/page.tsx
+++ app/(pages)/type/flying/page.tsx
@@ -1,10 +1,30 @@
-const PokemonTypeNormalPage = () => {
+'use client';
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import Error from './Error';
+import Pending from './Pending';
+import Result from './Result';
+const PokemonTypeFlyingPage = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['type'],
+    queryFn: async () => {
+      const id: string = 'flying';
+      const response = await axios.get(`https://pokeapi.co/api/v2/type/${id}`);
+      return response.data;
+    },
+  });
+  if (isLoading) return <Pending />;
+  if (error) return <Error />;
+  
   return (
-    <>
-      <h1>Normal Type</h1>
-      <h3>More Details Coming Soon.</h3>
-    </>
+    <section className='App container container-type-flying'>
+      <div className='row'>
+        <h1 className='responsive-h1 text-center gameboy title'>Flying Type</h1>
+      </div>
+      {data && <Result damage_relations={data} />}
+    </section>
   );
 };
 
-export default PokemonTypeNormalPage;
+export default PokemonTypeFlyingPage;
