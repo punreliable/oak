022129aa46d8b5a6d1dc702cf85efbd7ea2a023b
Brian <punreliable@gmail.com>
2025-08-05 17:24:54 -0400

    Rock Type Page Added

--- app/(pages)/type/rock/page.tsx
+++ app/(pages)/type/rock/page.tsx
@@ -1,10 +1,30 @@
-const PokemonTypeNormalPage = () => {
+'use client';
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import Error from './Error';
+import Pending from './Pending';
+import Result from './Result';
+const PokemonTypeBugPage = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['type'],
+    queryFn: async () => {
+      const id: string = 'rock';
+      const response = await axios.get(`https://pokeapi.co/api/v2/type/${id}`);
+      return response.data;
+    },
+  });
+  if (isLoading) return <Pending />;
+  if (error) return <Error />;
+
   return (
-    <>
-      <h1>Normal Type</h1>
-      <h3>More Details Coming Soon.</h3>
-    </>
+    <section className='App container container-type-rock'>
+      <div className='row'>
+        <h1 className='responsive-h1 text-center gameboy title'>Rock Type</h1>
+      </div>
+      {data && <Result damage_relations={data} />}
+    </section>
   );
 };
 
-export default PokemonTypeNormalPage;
+export default PokemonTypeBugPage;
