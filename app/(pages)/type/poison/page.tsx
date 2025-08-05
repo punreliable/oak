2d551b6c573fa3f207878ef60151a88a1e8aae7a
Brian <punreliable@gmail.com>
2025-08-05 17:31:55 -0400

    Poison page is back

---
+++ app/(pages)/type/poison/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import Error from './Error';
+import Pending from './Pending';
+import Result from './Result';
+const PokemonTypePoisonPage = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['type'],
+    queryFn: async () => {
+      const id: string = 'poison';
+      const response = await axios.get(`https://pokeapi.co/api/v2/type/${id}`);
+      return response.data;
+    },
+  });
+  if (isLoading) return <Pending />;
+  if (error) return <Error />;
+
+  return (
+    <section className='App container container-type-poison'>
+      <div className='row'>
+        <h1 className='responsive-h1 text-center gameboy title'>Poison Type</h1>
+      </div>
+      {data && <Result damage_relations={data} />}
+    </section>
+  );
+};
+
+export default PokemonTypePoisonPage;
