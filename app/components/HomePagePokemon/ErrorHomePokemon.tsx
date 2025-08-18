e0693063a86f8238683b372d0483332ad637d8bb
Brian <punreliable@gmail.com>
2025-08-18 12:48:19 -0400

    I think all code is back and errors have been resolved

--- app/components/HomePagePokemon/ErrorHomePokemon.tsx
+++ app/components/HomePagePokemon/ErrorHomePokemon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-import unknown from '../../../assets/0.png';
-import professorOak from '../../../assets/oak.svg';
+import unknown from '@/assets/0.png';
+import professorOak from '@/assets/oak.svg';
 
 const ErrorHomePokemon = () => {
   return (
