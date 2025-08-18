1973d7c0bfe957ca670cda58113beddb58885bc8
Brian <punreliable@gmail.com>
2025-08-18 08:03:19 -0400

    Removed errors from Move page

--- app/(pages)/move/ResultMove.tsx
+++ app/(pages)/move/ResultMove.tsx
@@ -5,28 +5,30 @@ import { dashRemover } from '../../../utilities/dashRemover';
 const ResultMove = (props: any) => {
   return (
     <div className='App'>
-      <h1 className='pokemonName'>{prettyName(props?.move.name)}</h1>
-      <div className='row rowStats'>
-        <div className='col-md-12 col-lg-12 stats'>
-          {props.move.flavor_text_entries && (
-            <h2 className='my-4'>{props.move.flavor_text_entries[0].flavor_text}</h2>
-          )}
-          {props.move.effect_entries && (
-            <h3 className='my-4'>{props.move.effect_entries[0].effect}</h3>
-          )}
-          {props.move.power && <p>Power: {props.move.power}</p>}
-          {props.move.pp && <p>PP: {props.move.pp}</p>}
-          {props.move.accuracy && <p>Accuracy: {props.move.accuracy}</p>}
-          {props.move.effect_chance && <p>Effect Chance: {props.move.effect_chance}&#37;</p>}
-          {props.move.damage_class && (
-            <p>Damage Class: {prettyName(props.move.damage_class.name)}</p>
-          )}
-          {props.move.type && <p>Type: {prettyName(props.move.type.name)}</p>}
-          {props.move.generation && (
-            <p>Generation: {dashRemover(prettyName(props.move.generation.name))}</p>
-          )}
+      <section className='nesContainer nes-container'>
+        <h1 className='pokemonName'>{prettyName(props?.move.name)}</h1>
+        <div className='row rowStats'>
+          <div className='col-md-12 col-lg-12 stats'>
+            {props.move.flavor_text_entries && (
+              <h2 className='my-4'>{props.move.flavor_text_entries[0].flavor_text}</h2>
+            )}
+            {props.move.effect_entries && (
+              <h3 className='my-4'>{props.move.effect_entries[0].effect}</h3>
+            )}
+            {props.move.power && <p>Power: {props.move.power}</p>}
+            {props.move.pp && <p>PP: {props.move.pp}</p>}
+            {props.move.accuracy && <p>Accuracy: {props.move.accuracy}</p>}
+            {props.move.effect_chance && <p>Effect Chance: {props.move.effect_chance}&#37;</p>}
+            {props.move.damage_class && (
+              <p>Damage Class: {prettyName(props.move.damage_class.name)}</p>
+            )}
+            {props.move.type && <p>Type: {prettyName(props.move.type.name)}</p>}
+            {props.move.generation && (
+              <p>Generation: {dashRemover(prettyName(props.move.generation.name))}</p>
+            )}
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
--- app/(pages)/move/page.tsx
+++ app/(pages)/move/page.tsx
@@ -1,40 +1,27 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+// import { useRouter } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import ErrorMove from './ErrorMove';
 import PendingMove from './PendingMove';
-import Result from './ResultMove';
+import ResultMove from './ResultMove';
 
-interface MovePageProps {
-  moveID: string; // Define the type of the prop
-}
+const MovePage = () => {
 
-const MovePage = ({ moveID }: MovePageProps) => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+  const moveID = 'cut';
   const { data, error, isLoading } = useQuery({
-    queryKey: ['ability', moveID],
+    queryKey: ['move', moveID],
     queryFn: async () => {
       const response = await axios.get(`https://pokeapi.co/api/v2/move/${moveID}`);
       return response.data;
     },
   });
-  if (!isClient) {
-    return null;
-  }
+
   if (isLoading) return <PendingMove />;
-
   if (error) return <ErrorMove />;
-
-  return (
-    <div className='App'>
-      <section className='nesContainer nes-container'>{data && <Result move={data} />}</section>
-    </div>
-  );
+  if (data) return <ResultMove move={data} />;
+  
 };
 
 export default MovePage;
