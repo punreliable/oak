411af59d94b1903a3db59582aa628972fd5ccb07
Brian <punreliable@gmail.com>
2025-08-20 11:46:26 -0400

    I am getting abilities via their id

--- app/(pages)/ability/[name]/page.tsx
+++ app/(pages)/ability/[name]/page.tsx
@@ -1,20 +1,37 @@
+import prettyName from '@/utilities/prettyName';
 
-interface Post {
-  id: string;
-  title: string;
-  content: string;
-}
+// interface Post {
+//   id: string;
+//   title?: string;
+//   content?: string;
+//   name: string;
+//   count: number;
+//   next: string | null;
+//   previous: string | null;
+//   results: Ability[];
+// }
 
-interface Ability {
+// interface Ability {
+//   name: string;
+//   url: string;
+// }
+
+
+
+interface Results {
   name: string;
   url: string;
-}
+}[];
 
 interface AbilityListResponse {
   count: number;
   next: string | null;
   previous: string | null;
-  results: Ability[];
+  results: Results;
+  // results: [{
+  //   name:string;
+  //   url: string;
+  // }];
 }
 
 
@@ -24,34 +41,32 @@ interface AbilityListResponse {
 export const revalidate = 60;
 
 export async function generateStaticParams() {
-  const posts: Post[] = await fetch('https://api.vercel.app/blog').then((res) => res.json());
-  return posts.map((post) => ({
-    name: String(post.id),
+  const abilityList: AbilityListResponse = await fetch(`https://pokeapi.co/api/v2/ability/`).then((res) => res.json());
+  return abilityList.results.map((result) => ({
+    name: String(result.name),
   }));
 }
 
 export default async function Page({ params }: { params: any }) {
 
   console.log('Hello World!');
-  // const { id } = await params;
-  const id = 1;
+  const { name } = await params;
+  // const id = 1;
 
-  console.log('ID: ', id)
+  console.log('ID: ', name)
 
-  const post: Post = await fetch(`https://api.vercel.app/blog/${id}`).then((res) => res.json());
-  console.log('Post: ', post);
-
+  // const post: Post = await fetch(`https://api.vercel.app/blog/${id}`).then((res) => res.json());
+  const post: AbilityListResponse = await fetch(`https://pokeapi.co/api/v2/ability/${name}`).then((res) => res.json());
+  // console.log('Post: ', post);
+  console.log('Post: ', prettyName(post.name));
   return (
     <main className="App">
       <div className="row">
-        <div className="col-md-6">
-          <h1 className="darkest-green">Hello</h1>
-          <h2 className="darkest-green">Title:</h2>
-          <p className="darkest-green">Content:</p>
-        </div>
-        <div className="col-md-6">
-          {post.title && <h1 className="darkest-green">{post.title}</h1>}
-          {post.content && <h1 className="darkest-green">{post.content}</h1>}
+      <h1 className="darkest-green">Ability</h1></div>
+      <div className="row">
+        <div className="col-md-12">
+          {post.name && <h1 className="darkest-green">Name: {prettyName(post.name)}</h1>}
+          {post.content && <p className="darkest-green">Content: {post.content}</p>}
         </div>
       </div>
     </main>
