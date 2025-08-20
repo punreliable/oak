84402ec6d30d80b3a194ea042787a5384268c819
Brian <punreliable@gmail.com>
2025-08-19 23:03:55 -0400

    Footer update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,29 +1,25 @@
 import Link from 'next/link';
 
 const Footer = () => {
+  const social: { facebook: string; github: string; instagram: string } = {
+    facebook: 'https://facebook.com/punreliable',
+    github: 'https://github.com/punreliable',
+    instagram: 'https://instagram.com/punreliable',
+  };
+
   return (
     <footer className='page-footer fixed-bottom gameboy text-center'>
       <div className='row mb-3'>
         <div className='col-md-12 footer-text'>
           <ul className='list-unstyled d-flex justify-content-center mb-0 py-4'>
             <li>
-              <Link href='http://like.comicui.com' target='_blank' className='icons-sm fb-ic'>
+              <Link href={social.facebook} target='_blank' className='icons-sm fb-ic'>
                 <i className='fab fa-facebook-f white-text fa-lg'> </i>
               </Link>
             </li>
             <li>
-              <Link href='http://code.comicui.com/' target='_blank' className='icons-sm github-ic'>
+              <Link href={social.github} target='_blank' className='icons-sm github-ic'>
                 <i className='fab fa-github white-text fa-lg'> </i>
-              </Link>
-            </li>
-            <li>
-              <Link href='http://bird.comicui.com' target='_blank' className='icons-sm tw-ic'>
-                <i className='fab fa-twitter white-text fa-lg'> </i>
-              </Link>
-            </li>
-            <li>
-              <Link href='http://view.comicui.com' target='_blank' className='icons-sm yt-ic'>
-                <i className='fab fa-youtube white-text fa-lg'> </i>
               </Link>
             </li>
           </ul>
@@ -32,8 +28,8 @@ const Footer = () => {
       <div className='row mb-3'>
         <div className='col-md-12 footer-text'>
           &copy;&nbsp;2025&nbsp;
-          <Link href='http://ComicUI.com' target='_blank'>
-            ComicUI.com
+          <Link href='http://punreliable.com' target='_blank'>
+            Punreliable.com
           </Link>
         </div>
       </div>
