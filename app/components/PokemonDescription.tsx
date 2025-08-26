ab69a98d2842a6c09bf7b57810e405bdf92b5abb
Brian <punreliable@gmail.com>
2025-08-26 11:16:55 -0400

    Pokemon Description should always be English and Red or Blue Pokedex Description

--- app/components/PokemonDescription.tsx
+++ app/components/PokemonDescription.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import prettyName from '@/utilities/prettyName';
 
 interface PokemonDescriptionProps {
   pokemon: number;
@@ -21,10 +22,22 @@ const style: React.CSSProperties = {
 const PokemonDescription: React.FC<PokemonDescriptionProps> = ({ pokemon }) => {
   const requestURL = `https://pokeapi.co/api/v2/pokemon-species/${parseInt(pokemon.toString())}/`;
 
-  console.log(requestURL);
+  // console.log(requestURL);
   const { data, error, isLoading, isError } = useQuery({
     queryKey: ['pokemon-species', requestURL],
     queryFn: () => fetchPokemonBaseHappiness(requestURL),
+  });
+
+  const description = data?.data.flavor_text_entries.map((flavor: 
+    {
+      flavor_text: string; 
+      language: {name: string; url: string;}; 
+      version: {name: string; url:string;};
+    }
+  ) => {
+    if (flavor.language.name == 'en' && (flavor.version.name === 'red' || flavor.version.name === 'blue' ) ) {
+      return flavor.flavor_text;
+    }
   });
 
   if (isLoading) {
@@ -34,17 +47,15 @@ const PokemonDescription: React.FC<PokemonDescriptionProps> = ({ pokemon }) => {
   if (isError) {
     return <div>Error: {error.message}</div>;
   }
-
+    
   return (
     <div className='col-md-12' style={style}>
-      {data && (
-        <>
-          <p>Description: {data.data.flavor_text_entries[0].flavor_text}</p>
-          <p>Base Happiness: {data.data.base_happiness}</p>
-        </>
-      )}
+      <p>Description: {description}</p>
+      <p>Base Happiness: {data?.data.base_happiness}</p>
+      <p>Capture Rate: {data?.data.capture_rate}</p>
+      <p>Color: {prettyName(data?.data.color.name)}</p>
     </div>
-  );
+  )
 };
 
 export default PokemonDescription;
