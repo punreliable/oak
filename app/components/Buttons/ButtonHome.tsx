c56b2e1c97d81da9ef3fb854a6f5ac3770f52b99
Brian <punreliable@gmail.com>
2025-08-27 20:54:11 -0400

    Added Suspense to ButtonHome component

--- app/components/Buttons/ButtonHome.tsx
+++ app/components/Buttons/ButtonHome.tsx
@@ -1,15 +1,18 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 const ButtonHome = () => {
   const searchParams = useSearchParams(); // Use the hook
   const from = searchParams.get('from');
   return (
-    <div className='col-md-12 my-4'>
-      <a href={`/pokemon/${from}`} className={`nes-btn is-warning`}>
-        Home
-      </a>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className='col-md-12 my-4'>
+        <a href={`/pokemon/${from}`} className={`nes-btn is-warning`}>
+          Home
+        </a>
+      </div>
+    </Suspense>
   );
 };
 
