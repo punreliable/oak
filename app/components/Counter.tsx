3953d2c5e1a711df0078a3302e887abada450f3e
Brian <punreliable@gmail.com>
2025-09-21 11:14:30 -0400

    Formatted and Linted

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -22,24 +22,32 @@ const Counter = () => {
 			<div className='row' style={{ display: 'block', width: '100%' }}>
 				<div className='col-md-12'>Value: {counter}</div>
 			</div>
-			<div className='row' style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', width: '100%', marginTop: '2rem'}}>
-
-				<div style={{width: '50%', maxWidth: '100%'}}>
+			<div
+				className='row'
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'space-around',
+					width: '100%',
+					marginTop: '2rem',
+				}}
+			>
+				<div style={{ width: '50%', maxWidth: '100%' }}>
 					<button onClick={incrementHandler} className={`nes-btn is-success`}>
 						Increment
 					</button>
 				</div>
-				<div style={{width: '50%', maxWidth: '100%'}}>
+				<div style={{ width: '50%', maxWidth: '100%' }}>
 					<button onClick={decrementHandler} className={`nes-btn is-error`}>
 						Decrement
 					</button>
 				</div>
 			</div>
-			<div style={{width: '100%', maxWidth: '100%', marginTop: '2rem'}}>
-					<button onClick={toggleCounterHandler} className={`nes-btn is-warning`}>
-						Toggle
-					</button>
-				</div>
+			<div style={{ width: '100%', maxWidth: '100%', marginTop: '2rem' }}>
+				<button onClick={toggleCounterHandler} className={`nes-btn is-warning`}>
+					Toggle
+				</button>
+			</div>
 		</div>
 	);
 };
