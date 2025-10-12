a3e147e542e97608573fbbc422e4598989a014cd
Brian <punreliable@gmail.com>
2025-10-12 16:01:21 -0400

    Removed unused console.log

--- app/components/PokemonLocationAreaEncounters.tsx
+++ app/components/PokemonLocationAreaEncounters.tsx
@@ -1,24 +1,16 @@
 'use client';
 import { v4 as uuidv4 } from 'uuid';
-import { LocationAreaEncounter } from '@/types/locationAreaEncounter';
 import { usePLAE } from '@/app/queries/usePLAE';
 import transformWords from '@/utilities/transformWords';
 import PendingPokeball from '@/app/components/PendingPokeball';
 
 const PokemonLocationAreaEncounters = (params: { locations: string }) => {
-	type LocationAreaEncounterFromAPI = {
-		data: LocationAreaEncounter;
-		result: number;
-	};
-
 	type LocationAreaEncounterResponse = {
 		location_area: { name: string };
 		version_details: { encounter_details: { version: { name: string } } };
 	};
 
 	const { data, isLoading } = usePLAE(params.locations);
-	const locationsData: LocationAreaEncounterFromAPI[] = data?.data;
-	console.log('Data: ', locationsData);
 	const locationList = data?.data.map((location: LocationAreaEncounterResponse) => {
 		return <li key={uuidv4()}>{transformWords(location.location_area.name)}</li>;
 	});
