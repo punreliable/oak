b7ee9bcb9a8fb89795c234f415ac3abee3d9e8ad
Brian <punreliable@gmail.com>
2025-10-13 12:31:49 -0400

    New Hook created, not implemented. Hoping to use mutations and/or selectors to solve locations encounter issues

---
+++ app/queries/useLocationVersions.ts
@@ -0,0 +1,8 @@
+import { useQuery, queryOptions } from '@tanstack/react-query';
+import axios from 'axios';
+
+export const useKantoLocations = (url: string) =>
+	queryOptions({
+		queryKey: ['locations', 'inKanto'],
+		queryFn: () => axios.get(url),
+	});
