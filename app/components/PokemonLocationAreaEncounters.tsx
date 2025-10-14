bdffe433ca6fe3d8ecf5b8d8a980f8aaf2e6f11a
Brian <punreliable@gmail.com>
2025-10-14 15:01:55 -0400

    Location Encounters use table, not unordered list

--- app/components/PokemonLocationAreaEncounters.tsx
+++ app/components/PokemonLocationAreaEncounters.tsx
@@ -18,7 +18,18 @@ const PokemonLocationAreaEncounters = (params: { locations: string }) => {
 	return (
 		<div className='row' style={{ display: 'inline' }}>
 			<h3 className='responsive-h3'>Location Encounters</h3>
-			<ul>{locationList}</ul>
+
+			<div className='nes-table-responsive'>
+				<table className='nes-table is-bordered is-centered is-dark'>
+					<thead>
+						<tr>
+							<th>Location Name</th>
+							<th className='hide-on-small'>Encounter Chance</th>
+						</tr>
+					</thead>
+					<tbody>{locationList}</tbody>
+				</table>
+			</div>
 		</div>
 	);
 
@@ -32,23 +43,28 @@ const PokemonLocationAreaEncounters = (params: { locations: string }) => {
 			)
 			.map((location: PokemonLocationAreaEncounter) => {
 				return (
-					<li key={uuidv4()}>
-						<Link href={getURLtoPath(location.location_area.url)}>
-							{transformWords(location.location_area.name)}
-						</Link>
-						<ul>
-							{location.version_details
-								.filter((versionDetail) =>
-									validVersions.includes(versionDetail.version.name),
-								)
-								.map((versionDetail, index) => (
-									<li key={index}>
-										Version: {transformWords(versionDetail.version.name)} - Max
-										Chance: {versionDetail.max_chance}%
-									</li>
-								))}
-						</ul>
-					</li>
+					<tr key={uuidv4()}>
+						<td>
+							<Link href={getURLtoPath(location.location_area.url)}>
+								{transformWords(location.location_area.name)}
+							</Link>
+						</td>
+						<td className='hide-on-small'>
+							<ul>
+								{location.version_details
+									.filter((versionDetail) =>
+										validVersions.includes(versionDetail.version.name),
+									)
+									.map((versionDetail, index) => (
+										<li key={index}>
+											Version: {transformWords(versionDetail.version.name)}{' '}
+											<br />
+											Max Chance: {versionDetail.max_chance}%
+										</li>
+									))}
+							</ul>
+						</td>
+					</tr>
 				);
 			});
 	}
