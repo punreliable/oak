622121eb5a4cdc8e46cd334cbb2695bc86dac448
Brian Smith <brian@Brians-Mac-mini.local>
2026-05-09 10:00:53 -0400

    Fixed syntax error in components/PokemonMoveList with Copilot

--- app/components/PokemonMoveList.tsx
+++ app/components/PokemonMoveList.tsx
@@ -6,16 +6,17 @@ import type { MoveList } from '@/types/move-list';
 const PokemonMoveList = (props: MoveList) => {
 	const moves = props.moves;
 	const fromPage = props.from;
-	const minified = moves.map((x: any) => (
-
-		if( x.version_group_details[0].move_learn_method.name !== 'level-up' ) { {
-			<div className='col-md-3 px-2 py-4' style={{ display: 'inline' }} key={uuidv4()}>
-				<a href={`/move/${x.move.name}?from=${fromPage}`} className={`nes-btn is-error my-1`}>
-					{prettyName(x.move.name)}
-				</a>
-			</div>
+	const minified = moves.map((x: any) => {
+		if( x.version_group_details[0].move_learn_method.name !== 'level-up' ) {
+			return (
+				<div className='col-md-3 px-2 py-4' style={{ display: 'inline' }} key={uuidv4()}>
+					<a href={`/move/${x.move.name}?from=${fromPage}`} className={`nes-btn is-error my-1`}>
+						{prettyName(x.move.name)}
+					</a>
+				</div>
+			);
 		}
-	));
+	});
 	return <>{minified}</>;
 };
 
